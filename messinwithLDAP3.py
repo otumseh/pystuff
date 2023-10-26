f8adf37f6149a0333f8c0034d8b86a68cdb7bb11
otumseh <66980589+otumseh@users.noreply.github.com>
2023-10-26 09:25:17 -0400

    Add files via upload

---
+++ messinwithLDAP3.py
@@ -0,0 +1,11 @@
+
+from ldap3 import Server, Connection, ALL
+
+
+server = Server('ssi-mi.local', get_info=ALL)
+conn = Connection(server)
+conn.bind()
+serv = server.info
+
+print(conn)
+print(serv)
